b155e792bc316ef45c701e8fce894996c32e7f83
RDZ Gossau <94575224+RDZGossau@users.noreply.github.com>
2022-04-04 14:55:37 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+let strip = neopixel.create(DigitalPin.P2, 30, NeoPixelMode.RGB)
+strip.showRainbow(1, 360)
+basic.pause(2000)
+strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
+basic.pause(2000)
+strip.clear()
+strip.show()
 basic.forever(function () {
-	
+    for (let LEDNr = 0; LEDNr <= 30; LEDNr++) {
+        strip.setPixelColor(LEDNr, neopixel.colors(NeoPixelColors.Yellow))
+        basic.pause(500)
+    }
 })
